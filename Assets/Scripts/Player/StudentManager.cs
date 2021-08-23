39b0dc519ef646de66ca10ba9d6123f80ebe8e2b
Konstantin Milev <milev109@gmail.com>
2021-08-23 14:53:07 +0300

    Hook student count with UI, add functions SpawnLeader and AddStudent

--- Assets/Scripts/Player/StudentManager.cs
+++ Assets/Scripts/Player/StudentManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class StudentManager : MonoBehaviour
 {
@@ -8,9 +9,28 @@ public class StudentManager : MonoBehaviour
 
     public List<Student> students = new List<Student>();
 
+    public TMP_Text studentNumberText;
+
     private void Awake()
     {
-        Instantiate(studentPrefab, Vector3.zero, Quaternion.identity);
+        SpawnLeader();
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void SpawnLeader()
+    {
+        Transform studentToAdd = Instantiate(studentPrefab, Vector3.zero, Quaternion.identity);
+        AddStudent(studentToAdd.GetComponent<Student>());
+    }
+
+    private void AddStudent(Student student)
+    {
+        students.Add(student);
+        studentNumberText.text = $"Students: {GetStudentCount()}";
     }
 
     public int GetStudentCount() => students.Count;
