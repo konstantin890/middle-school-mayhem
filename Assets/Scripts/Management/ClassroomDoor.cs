8b912a4b099e3180149fef2dff12cc6397b88d99
Konstantin Milev <milev109@gmail.com>
2021-08-27 01:37:23 +0300

    Prepare animations for level fading

--- Assets/Scripts/Management/ClassroomDoor.cs
+++ Assets/Scripts/Management/ClassroomDoor.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class ClassroomDoor : MonoBehaviour
 {
-    public bool toUpperFloor;
-    public int currentFloor;
-    public int roomNumber;
+    public string sceneNameEnter;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("StudentLeader"))
         {
             Debug.Log("Student Enter!");
+            SceneHandler.instance.GoToScene(sceneNameEnter);
         }
     }
 }
--- Assets/Scripts/Management/SceneHandler.cs
+++ Assets/Scripts/Management/SceneHandler.cs
@@ -2,18 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneHandler : MonoBehaviour
 {
     public static SceneHandler instance;
+
+    private string currentlyLoadedLevelName;
+
+    public string initialSceneName;
 
     public int floorOneHallSceneId;
     public int[] floorOneSceneIds;
     public int floorTwoHallSceneId;
     public int[] floorTwoSceneIds;
 
+    public float fadeMultiplier;
+
+    public Animator fadeAnimator;
+
     private void Awake()
     {
         instance = this;
+
+        currentlyLoadedLevelName = initialSceneName;
+        SceneManager.LoadScene(initialSceneName);
+    }
+
+    public void GoToScene(string sceneName)
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName);
+        fadeAnimator.SetTrigger("FadeOut");
+        currentlyLoadedLevelName = sceneName;
+    }
+
+    public void OnFadeOutComplete()
+    {
+        StartCoroutine(LoadSceneAsync());
+    }
+
+    IEnumerator LoadSceneAsync()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(currentlyLoadedLevelName); // in this case currentlyLoadedLevelName is the scene to be loaded next
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
     }
 }
