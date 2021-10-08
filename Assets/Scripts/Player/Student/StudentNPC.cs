b83f1dc0dbfccc74077279389f6f563f91f75094
Konstantin Milev <milev109@gmail.com>
2021-10-09 00:43:45 +0300

    Add license header to all scripts and put the template in .editorconfig

--- Assets/Scripts/Player/Student/StudentNPC.cs
+++ Assets/Scripts/Player/Student/StudentNPC.cs
@@ -1,3 +1,11 @@
+// Developed by TerraStudios (https://github.com/TerraStudios)
+//
+// Copyright(c) 2021 Konstantin Milev (konstantin890 | milev109@gmail.com)
+// Copyright(c) 2021 Nikos Konstantinou (nikoskon2003)
+//
+// The following script has been written by either konstantin890 or Nikos (nikoskon2003) or both.
+// This file is covered by the GNU GPL v3 license. Read LICENSE.md for more information.
+
 using Pathfinding;
 using System.Collections;
 using System.Collections.Generic;
